dddcf674083ff7ca4bc8b44c4f1d8d9f93b96bc4
shanekang <shane.kang@outlook.com>
2020-07-26 17:35:57 +0900

    Answer quiz 04

---
+++ quiz/quiz4_2_bubble_sort.py
@@ -0,0 +1,8 @@
+def bubble_sort(list):
+    for i in range(len(list)-1, 0, -1):
+        for j in range(i):
+            if list[j] > list[j+1]:
+                list[j], list[j+1] = list[j+1], list[j]
+    return list
+list = [7,4,5,1,3]
+print(bubble_sort(list))
